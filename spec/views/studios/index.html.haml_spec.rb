a0abecabf22866ba52185ce1adca1fc072c9906f
Marshall Yount <marshall@yountlabs.com>
2012-01-17 20:31:15 -0600

    remove fluff from Studios#index

--- spec/views/studios/index.html.haml_spec.rb
+++ spec/views/studios/index.html.haml_spec.rb
@@ -24,17 +24,9 @@ describe "studios/index" do
 
   it "renders a list of studios" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Address".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.5.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 2.5.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Photo Url".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Studio Url".to_s, :count => 2
+    assert_select "tr>td", :text => "Calendar".to_s, :count => 2
   end
 end
