4dd0561858578341e82efe188cda7ca4cec2e0cf
Marshall Yount <marshall@yountlabs.com>
2015-03-08 17:11:49 -0500

    update vcr cassettes with new sunstone format

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -5,11 +5,11 @@ describe Studio do
     it "should crawl studios" do
       VCR.use_cassette('studio/locations') do
         Studio.crawl_studios
-        s = Studio.first
+        s = Studio.find_by_slug("north_hills_center")
         s.name.should == "North Hills Center"
         s.address.include?("10710 Research Blvd, #326").should == true
         s.studio_url.should == "http://www.sunstoneyoga.com/nhc"
-        Studio.count.should == 13
+        Studio.count.should == 14
       end
     end
 
