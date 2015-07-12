61399e092d9cac4f0bacddaa6196fc6d6ccd278b
Marshall Yount <marshall@yountlabs.com>
2015-07-12 16:55:21 -0500

    add a spec for StudiosController

--- spec/controllers/studios_controller_spec.rb
+++ spec/controllers/studios_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe StudiosController do
+  let(:studio) { Studio.create! slug: "the_studio" }
+
+  describe "GET show" do
+    it "assigns the requested studio as @studio" do
+      get :show, { id: studio.to_param }
+      assigns(:studio).should eq(studio)
+    end
+  end
 end
