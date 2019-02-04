03683da8e44af3a7666864421b668843cc0a6b77
Marshall Yount <myount@twilio.com>
2019-02-04 06:19:23 -0600

    remove config.whiny_nils

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,6 @@
 SunstoneCal::Application.configure do
   config.cache_classes = false
 
-  config.whiny_nils = true
-
   config.consider_all_requests_local       = true
 
   config.action_controller.perform_caching = false
